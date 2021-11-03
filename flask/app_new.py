f4c69eb1027e8957246fc667d301efd997451753
Christian Alejandro Mosri <39068565+IngMosri@users.noreply.github.com>
2021-11-03 11:24:09 -0600

    Update app_new.py

--- flask/app_new.py
+++ flask/app_new.py
@@ -34,7 +34,53 @@ def index():
 
 @app.route('/create_student/', methods=['POST'])
 def create_student():
-    return "Estudent created"
+    name = request.json['name']
+    #print(name)
+    career = request.json['career']
+    #print(career)
+    new_student = Student(name,career)
+    print(new_student)
+    
+    database.session.add(new_student)
+    database.session.commit()
+    
+    #return "student created 102621"
+    return students_schema.jsonify(new_student)
+
+@app.route('/student/', methods=['GET'])
+def get_students():
+    all_students = Student.query.all()
+    result = students_schema.dump(all_students)
+    return jsonify(result)
+
+@app.route('/students/<id>', methods=['GET'])
+def get_student(id):
+    student = Student.query(id)
+    #return id
+    return students_schema.jsonify(student)
+
+@app.route('/students_update/<id>/', methods=['PUT'])
+def student_update(id):
+    update = Student.query.get(id)
+    name = request.json['name']
+    career = request.json['career']
+    
+    update.name = name
+    update.career = career
+    database.session.commit()
+    return students_schema.jsonify(update)
+
+@app.route('/student_delete/<id>/', methods = ['DELETE'])
+def student_delete(id):
+    delete = Student.query.get(id)
+    database.session.delete(delete)
+    database.session.commit()
+    return students_schema.jsonify(delete)
+    
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
